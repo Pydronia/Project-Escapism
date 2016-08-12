8b8de892aa30f39f52ca3cdd3568899794697743
Pydronia <jackscmail@gmail.com>
2016-08-12 15:56:23 +1000

    Add Unity Project

---
+++ Project Escapism V0.3/Assets/Scripts/Pointer.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour {
+	
+	//Vive button setup
+	//private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	//private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	//private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+	//SteamVR_TrackedObject controller;
+
+	public GameObject placedObj;
+
+	public float rotationSpeed = 1.0f;
+
+	public float laserWidth = 0.01F;
+	public Material LaserEffect;
+
+	GameObject manipObj;
+	GameObject[] existObj;
+
+	public bool alreadyInst = false;
+
+	LineRenderer myLine;
+	RaycastHit hitPos;
+	Quaternion rotation;
+
+	Vector3 placingPos;
+	Vector3 placingExt;
+	Vector3 shiftDir;
+
+	int layerMask = 1 << 8;
+
+	public bool firstTime = true;
+
+
+
+	void Start () {
+
+		//controller = GetComponent<SteamVR_TrackedObject> ();
+		myLine = gameObject.AddComponent<LineRenderer> ();
+		myLine.SetWidth (laserWidth, laserWidth);
+		myLine.enabled = false;
+		myLine.material = LaserEffect;
+		layerMask = ~layerMask;
+
+
+	}
+
+	void Update () {
+		//steamVR Controller setup
+		//SteamVR_Controller.Device device = SteamVR_Controller.Input((int)controller.index);
+
+		//raycasting/laser
+
+		Ray pointerRay = new Ray (this.transform.position, this.transform.forward);
+		bool hit = Physics.Raycast (pointerRay, out hitPos, 100, layerMask);
+		if (hit) {
+			if (hitPos.collider.tag == "environment" || hitPos.collider.tag == "object") {
+				myLine.enabled = true;
+				myLine.SetPosition (0, transform.position);
+				myLine.SetPosition (1, hitPos.point);
+			} else {
+				myLine.enabled = false;
+			}
+		} else {
+			myLine.enabled = false;
+		}
+
+		//if there's a object selected, it's not been instantiated, raycast hits something, and what it's hitting is environment
+		if (placedObj) {
+			if (alreadyInst == false) {
+				if (hit) {
+					if (hitPos.collider.tag == "environment" || hitPos.collider.tag == "object") {
+						if (firstTime) {
+							shift ();
+							manipObj = (GameObject)Instantiate (placedObj, shift(), Quaternion.identity);
+							firstTime = false;
+						} else {
+							manipObj = (GameObject)Instantiate (placedObj, shift(), rotation);
+						}
+
+						manipObj.tag = "manip";
+						alreadyInst = true;
+					} //TODO: add else here to prevent placing of objects on things other than just objects and evnironment.
+				}
+			} else if (!hit) {//if it's not hitting anything, save rotation, destroy it and reinstantiate it when it hits something again
+				rotation = manipObj.transform.rotation;
+				Destroy (manipObj);
+				alreadyInst = false;
+			} else {//repositioning
+				reposition ();
+                /*
+				//rotate vive
+				Vector2 touchPos = device.GetAxis();
+				if (touchPos.x > 0.5 && device.GetPress (touchpad)) {
+					manipObj.transform.Rotate (Vector3.down * rotationSpeed, Space.Self);
+				}
+				if (touchPos.x < -0.5 && device.GetPress (touchpad)) {
+					manipObj.transform.Rotate (Vector3.up * rotationSpeed, Space.Self);
+				}
+
+				//Place the object! Vive
+				if (device.GetPressDown (triggerButton)) {
+					manipObj.tag = "object";
+					manipObj.layer = 0;
+					firstTime = true;
+					placedObj = null;
+					manipObj = null;
+					alreadyInst = false;
+				}
+                */
+				//rotate keyboard
+				if (Input.GetKey(KeyCode.Q)) {
+					manipObj.transform.Rotate (Vector3.up * rotationSpeed, Space.Self);
+				}
+				if (Input.GetKey (KeyCode.E)) {
+					manipObj.transform.Rotate (Vector3.down * rotationSpeed, Space.Self);
+				}
+
+				//Place the object! Keyboard
+				if (Input.GetKeyDown (KeyCode.P)) {
+					manipObj.tag = "object";
+					manipObj.layer = 0;
+					firstTime = true;
+					placedObj = null;
+					manipObj = null;
+					alreadyInst = false;
+				}
+			}
+				
+		}
+
+		//delete the objects
+        /*
+		if (device.GetPressDown (gripButton)){
+			deleteObjs ();
+		}
+        */
+		if (Input.GetKeyDown (KeyCode.D)) {
+			deleteObjs ();
+		}
+
+	}
+
+
+	Vector3 shift(){
+		placingPos = hitPos.point;
+		placingExt = placedObj.GetComponent<MeshRenderer> ().bounds.extents;
+		shiftDir = hitPos.normal;
+		Vector3 temp;
+		int roundedx = (int)Mathf.RoundToInt (shiftDir.x);
+		int roundedy = (int)Mathf.RoundToInt (shiftDir.y);
+		int roundedz = (int)Mathf.RoundToInt (shiftDir.z);
+
+		if (roundedx != 0) {
+			if (roundedx == 1) {
+				//Debug.Log ("right");
+				temp = placingPos;
+				temp.x = placingPos.x + placingExt.x;
+				return(temp);
+			} else {
+				//Debug.Log ("left");
+				temp = placingPos;
+				temp.x = placingPos.x - placingExt.x;
+				return(temp);
+			}
+		} else if (roundedy != 0) {
+			if (roundedy == 1) {
+				//Debug.Log ("up");
+				temp = placingPos;
+				temp.y = placingPos.y + placingExt.y;
+				return(temp);
+			} else {
+				//Debug.Log ("down");
+				temp = placingPos;
+				temp.y = placingPos.y - placingExt.y;
+				return(temp);
+			}
+		} else {
+			if (roundedz == 1) {
+				//Debug.Log ("forward");
+				temp = placingPos;
+				temp.z = placingPos.z + placingExt.z;
+				return(temp);
+			} else {
+				//Debug.Log ("back");
+				temp = placingPos;
+				temp.z = placingPos.z - placingExt.z;
+				return(temp);
+			}
+		}
+
+	}
+
+	void reposition (){
+		manipObj.transform.position = shift();
+	}
+
+	void deleteObjs(){
+		existObj = GameObject.FindGameObjectsWithTag ("object");
+		for (int i = 0; i < existObj.Length; i++) {
+			Destroy (existObj [i]);
+		}
+	}
+}
---
+++ Project Escapism V0.3/Assets/Scripts/objectSelect.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectSelect : MonoBehaviour {
+	
+	//vive button setup
+	//private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	//private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	//SteamVR_TrackedObject controller;
+
+	public GameObject righthand;
+
+	GameObject toSend;
+
+	Pointer rightScript;
+
+	void Start () {
+		//controller = GetComponent<SteamVR_TrackedObject> ();
+		rightScript = righthand.GetComponent<Pointer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//steamVR Controller setup
+		//SteamVR_Controller.Device device = SteamVR_Controller.Input((int)controller.index);
+        /*
+		if (device.GetPressDown (triggerButton)) {
+			Destroy(GameObject.FindWithTag("manip"));
+			rightScript.alreadyInst = false;
+			rightScript.firstTime = true;
+			toSend = (GameObject)Resources.Load ("Prefabs/Sphere Test");
+			rightScript.placedObj = toSend;
+		}
+		if (device.GetPressDown (gripButton)){
+			Destroy(GameObject.FindWithTag("manip"));
+			rightScript.alreadyInst = false;
+			rightScript.firstTime = true;
+			toSend = (GameObject)Resources.Load ("Prefabs/Cube Test");
+			rightScript.placedObj = toSend;
+		}
+        */
+		//keyboard testing
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Destroy(GameObject.FindWithTag("manip"));
+			rightScript.alreadyInst = false;
+			rightScript.firstTime = true;
+			toSend = (GameObject)Resources.Load ("Prefabs/Sphere Test");
+			rightScript.placedObj = toSend;
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+			Destroy(GameObject.FindWithTag("manip"));
+			rightScript.alreadyInst = false;
+			rightScript.firstTime = true;
+			toSend = (GameObject)Resources.Load ("Prefabs/Cube Test");
+			rightScript.placedObj = toSend;
+		}
+	}
+}
