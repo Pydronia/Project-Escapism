ff1429c8d4910f81f2cd88c6b16b4339011fcfc8
Pydronia <jackscmail@gmail.com>
2016-08-25 20:04:31 +1000

    work on file browser/vive ui interaction

---
+++ 3Dception/Assets/Toggle3DCeption.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toggle3DCeption : MonoBehaviour {
+
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.T)) {
+			this.GetComponent<TBE.Components.TBE_SourceControl> ().enabled = !this.GetComponent<TBE.Components.TBE_SourceControl> ().enabled;
+		}
+	}
+}
